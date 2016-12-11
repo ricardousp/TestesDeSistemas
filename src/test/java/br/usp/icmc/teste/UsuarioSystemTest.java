9395598cafed3f3b500b5505cc0f5ab85e3f8811
ricardoramos <ricardoramos.usp@gmail.com>
2016-12-11 18:04:29 -0200

    Formatando o código da classe UsuarioSystemTest

--- src/test/java/br/usp/icmc/teste/UsuarioSystemTest.java
+++ src/test/java/br/usp/icmc/teste/UsuarioSystemTest.java
@@ -13,95 +13,81 @@ import br.usp.icmc.page.NovoUsuarioPage;
 import br.usp.icmc.util.WebDriverSingleton;
 
 public class UsuarioSystemTest {
-	
+
 	private WebDriver driver;
 	private ListarUsuariosPage usuariosPage;
-	
+
 	@Before
-	public void inicializa(){		
-		this.driver = WebDriverSingleton.get();	
+	public void inicializa() {
+		this.driver = WebDriverSingleton.get();
 		this.usuariosPage = new ListarUsuariosPage(driver);
 		driver.get("http://www.labes.icmc.usp.br:8788/apenas-teste/limpa");
 	}
-	
+
 	@Test
-	public void deveAdicionarUmUsuario(){	
-		
+	public void deveAdicionarUmUsuario() {
+
 		usuariosPage.visita();
 		usuariosPage.novo().cadastrar("Ricardo Ramos de Oliveira", "ricardoramos.usp@gmail.com");
 		assertTrue(usuariosPage.existeNaListagem("Ricardo Ramos de Oliveira", "ricardoramos.usp@gmail.com"));
-		
-	}
-	
-	/* Defeito encontrado na aplicação:
-	<table>
-	 	<tbody>
-			<tr>
-				<th>Nome</th>
-				<th>E-mail</th>
-				<th></th>
-			</tr>
-		</tbody>
-	</table>
 
-	* Forma Correta:	
-	<table>
-		<thead>
-			<tr>
-				<th>Nome</th>
-				<th>E-mail</th>
-				<th></th>
-			</tr>
-		</thead>
-		<tbody>
-		
-		</tbody>
-	</table>	
-	
-	Na prática deveríamos reportar esse bug e não deveríamos modificar o nosso testes, 
-	mas para efeitos didáticos vamos modificar o teste para ele poder passar...
-	
-	*/
-	
+	}
+
+	/*
+	 * Defeito encontrado na aplicação: <table> <tbody> <tr> <th>Nome</th>
+	 * <th>E-mail</th> <th></th> </tr> </tbody> </table>
+	 * 
+	 * Forma Correta: <table> <thead> <tr> <th>Nome</th> <th>E-mail</th>
+	 * <th></th> </tr> </thead> <tbody>
+	 * 
+	 * </tbody> </table>
+	 * 
+	 * Na prática deveríamos reportar esse bug e não deveríamos modificar o
+	 * nosso testes, mas para efeitos didáticos vamos modificar o teste para ele
+	 * poder passar...
+	 * 
+	 */
+
 	@Test
-	public void deveListarUsuarios(){				
+	public void deveListarUsuarios() {
 		usuariosPage.visita();
 		int quantidadeDeUsuarios = usuariosPage.getRegistrosDeUsuarios().size();
-		// assertEquals(0, quantidadeDeUsuarios); -> Assert Correto!	
-		assertEquals(1, quantidadeDeUsuarios);		
+		// assertEquals(0, quantidadeDeUsuarios); -> Assert Correto!
+		assertEquals(1, quantidadeDeUsuarios);
 	}
-	
+
 	@Test
-	public void deveRemoverUsuario(){
+	public void deveRemoverUsuario() {
 		usuariosPage.visita();
 		usuariosPage.novo().cadastrar("Ricardo Ramos de Oliveira", "ricardoramos.usp@gmail.com");
-		assertTrue(usuariosPage.existeNaListagem("Ricardo Ramos de Oliveira", "ricardoramos.usp@gmail.com"));		
+		assertTrue(usuariosPage.existeNaListagem("Ricardo Ramos de Oliveira", "ricardoramos.usp@gmail.com"));
 		usuariosPage.deletaUsuarioNaPosicao(1);
-		assertFalse(usuariosPage.existeNaListagem("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br"));		
+		assertFalse(usuariosPage.existeNaListagem("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br"));
 	}
-	
+
 	@Test
-	public void deveEditarUmUsuario(){	
+	public void deveEditarUmUsuario() {
 		usuariosPage.visita();
-		usuariosPage.novo().cadastrar("Paulo Henrique", "paulo@henrique.com");		
-		usuariosPage.alteraUsuarioNaPosicao(1).para("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br");;		
+		usuariosPage.novo().cadastrar("Paulo Henrique", "paulo@henrique.com");
+		usuariosPage.alteraUsuarioNaPosicao(1).para("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br");
+		;
 		assertFalse(usuariosPage.existeNaListagem("Paulo Henrique", "paulo@henrique.com"));
-		assertTrue(usuariosPage.existeNaListagem("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br"));  		
+		assertTrue(usuariosPage.existeNaListagem("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br"));
 	}
-	
+
 	@Test
-	public void naoDeveAdicionarUmUsuarioSemNome(){		    		
-		NovoUsuarioPage formulario = usuariosPage.novo();		
-		formulario.cadastrar("", "ricardoramos.usp@gmail.com");				     	                
-        assertTrue(formulario.verificaMsgExecaoNome());     		
+	public void naoDeveAdicionarUmUsuarioSemNome() {
+		NovoUsuarioPage formulario = usuariosPage.novo();
+		formulario.cadastrar("", "ricardoramos.usp@gmail.com");
+		assertTrue(formulario.verificaMsgExecaoNome());
 	}
-	
+
 	@Test
-	public void naoDeveAdicionarUmUsuarioSemNomeeSemEmail(){		
-		NovoUsuarioPage formulario = usuariosPage.novo();		
-		formulario.cadastrar("", "");						        
-        assertTrue(formulario.verificaMsgExecaoNome());
-        assertTrue(formulario.verificaMsgExecaoEmail());    	        	       
-	}		
-		
+	public void naoDeveAdicionarUmUsuarioSemNomeeSemEmail() {
+		NovoUsuarioPage formulario = usuariosPage.novo();
+		formulario.cadastrar("", "");
+		assertTrue(formulario.verificaMsgExecaoNome());
+		assertTrue(formulario.verificaMsgExecaoEmail());
+	}
+
 }
